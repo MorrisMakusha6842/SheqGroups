49fcc380f74647af5be45e75c1d4f296cdbf435a
unknown <morrismakusha23@gmail.com>
2024-07-07 15:22:32 +0200

    Added mixing component

---
+++ src/app/production/mixing/mixing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mixing',
+  standalone: true,
+  imports: [],
+  templateUrl: './mixing.component.html',
+  styleUrl: './mixing.component.scss'
+})
+export class MixingComponent {
+
+}
